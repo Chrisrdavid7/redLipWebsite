5cb0d4c4d2b9fde6638bdd3be263e2f822bd6280
“Chris <chrisrdavid7@gmail.com>
2024-09-16 17:12:44 -0400

    fixed

--- assets/text/brand.js
+++ assets/text/brand.js
@@ -1,12 +1,12 @@
 const brand = {
   movie: {
-    name: 'Chelsey Creative',
-    desc: 'Chelsey Creative - Original Casting and Development for Award-Winning Shows',
+    name: 'Red Lip Productions',
+    desc: 'Red Lip Productions - Original Casting and Development for Award-Winning Shows',
     prefix: ' ',
     footerText: '',
     logoText: '',
-    projectName: 'Chelsey Creative',
-    url: 'chelseycreative.com',
+    projectName: 'Red Lip Productions',
+    url: 'redlipproductions.com',
     img: '/images/movie-logo.png',
     notifMsg: 'Built by Chris David',
   },
